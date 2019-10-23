51a4ce480f7a3d840c94b00d889ad1308eb1e937
dani <dani.zaghian@gmail.com>
2019-10-23 10:53:48 -0700

    tests toggle complete method

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#toggle_complete!' do
+    it 'should switch complete to false if it began as true' do
+      task = Task.create(complete: true)
+      task.toggle_complete!
+      expect(task.complete).to eq(false)
+    end
+    it 'should switch complete to true if it began as false' do
+      task = Task.create(complete: false)
+      task.toggle_complete!
+      expect(task.complete).to eq(true)
+    end
+  end
+  describe '#toggle_favorite!' do
+    
+  end
 end
